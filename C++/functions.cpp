9f145969f16fef696b0065a0d485536adbe56b3b
D-ixon <dicksonsci@gmail.com>
2025-12-24 22:23:00 +0000

    References created and return values modified

---
+++ C++/functions.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+int doubleGame(int x) {
+  return x * 2;
+}
+
+void changeValue(int &num) {
+  num = 50;
+}
+
+void swapNums(int &x, int &y) {
+  int z = x;
+  x = y;
+  y = z;
+}
+
+int main() {
+  for (int i = 1; i <= 5; i++) {
+    cout << "Double of " << i << " is " << doubleGame(i) << endl;
+  }
+
+  int value = 10;
+  changeValue(value);  // Call the function and change the value to 50
+  cout << value <<endl;
+
+  int firstNum = 10;
+  int secondNum = 20;
+
+  cout << "Before swap: " << "\n";
+  cout << firstNum <<"  "<< secondNum << "\n";
+
+  // Call the function, which will change the values of firstNum and secondNum
+  swapNums(firstNum, secondNum);
+
+  cout << "After swap: " << "\n";
+  cout << firstNum <<" "<<secondNum << "\n";
+  return 0;
+}
