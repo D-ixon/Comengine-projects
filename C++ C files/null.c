93cc0087d0198a454b4755490d1f782fb67196e0
D-ixon <dicksonsci@gmail.com>
2026-02-19 21:05:59 +0000

    minor

---
+++ C++ C files/null.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+
+
+//Variable types 
+//Arrays 
+//Multidimensional Arrays
+
+
+int main(){
+    int numbers[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++){
+        printf( numbers[i]);
+    }
+   
+
+    return 0;
+}
