f71c00e48087e6dc7c60f0e2f8e90338d77bfd04
D-ixon <dicksonsci@gmail.com>
2026-02-23 15:46:27 +0000

    Adding and removing

--- C++ C files/null.c
+++ C++ C files/null.c
@@ -2,13 +2,15 @@
 
 int main() {
 
-    int myArray[] = {10, 20, 30, 40, 50}; 
+    int myArray[10] = {10, 20, 30, 40, 50}; 
+
+    myArray[5] = 60;
     
     int size = sizeof(myArray) / sizeof(myArray[0]);
 
-    printf("Elements in the array are: ");
+    printf("Elements in the array are: \n");
     for (int i = 0; i < size; i++) {
-        printf("%d ", myArray[i]); 
+        printf("%d\n", myArray[i]); 
     }
     
     printf("\n"); 
