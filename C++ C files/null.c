a379eff38391b945d3a9d09ce3b5c4cc4df5e3f5
D-ixon <dicksonsci@gmail.com>
2026-02-24 21:51:26 +0000

    Array mod

--- C++ C files/null.c
+++ C++ C files/null.c
@@ -2,18 +2,8 @@
 
 int main() {
 
-    int myArray[10] = {10, 20, 30, 40, 50}; 
+    printf("Hello \n");
 
-    myArray[5] = 60;
-    
-    int size = sizeof(myArray) / sizeof(myArray[0]);
 
-    printf("Elements in the array are: \n");
-    for (int i = 0; i < size; i++) {
-        printf("%d\n", myArray[i]); 
-    }
-    
-    printf("\n"); 
-    
     return 0;
 }
