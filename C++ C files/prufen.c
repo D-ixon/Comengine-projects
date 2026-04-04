4e134654740a7f4192ce2740ad8f7e4cf48d182d
Kofi Dixon <dicksonsci@gmail.com>
2026-04-04 08:24:12 +0000

    Swap modified

--- C++ C files/prufen.c
+++ C++ C files/prufen.c
@@ -4,7 +4,7 @@ int main() {
     int motor[5] = {10, 20, 30, 40, 50};
     int temp; 
 
-    printf("--- Original Motor Speeds ---\n");
+    printf("--- --Original Motor Speeds -- ---\n");
     for (int i = 0; i < 5; i++) {
         printf("Motor [%d]: %d RPM\n", i, motor[i]);
     }
@@ -14,7 +14,7 @@ int main() {
     motor[0] = motor[4];  
     motor[4] = temp;      
 
-    printf("\n[SYSTEM]: Swap complete. First and Last elements exchanged.\n\n");
+    printf("\n Swap complete. First and Last elements exchanged.\n\n");
 
     printf("--- Updated Motor Speeds ---\n");
     for (int i = 0; i < 5; i++) {
