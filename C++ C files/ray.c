b8938ac86d678b3dec96df344f152d90cf53dabc
Kofi Dixon <dicksonsci@gmail.com>
2026-04-07 21:18:43 +0000

    prinft isnt smart

--- C++ C files/ray.c
+++ C++ C files/ray.c
@@ -4,14 +4,7 @@ int main(){
     int bun[5] = {5, 5, 6, 6, 6};
 
     for(int i = 0; i < 5; i++){
-        // We add a second %d to catch the second variable (bun[i])
-        printf("Index [%d] | Value: %d\n", i, bun[i]);
-        
-        // We add a %d for the index and a %p for the address
-        printf("Index [%d] | Address: %p\n", i, (void*)&bun[i]);
-        
-        printf("--------------------------\n");
+        printf("The Values: %d\n", bun[i]);
+        printf("The address of this elements: %p\n", &bun[i]);
     }
-    
-    return 0;
 }
