58f94b70c59526516f894e0b5f58bc32452e0a00
Kofi Dixon <dicksonsci@gmail.com>
2026-04-07 21:13:59 +0000

    C Memory

---
+++ C++ C files/ray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int bun[5] = {5, 5, 6, 6, 6};
+
+    for(int i = 0; i < 5; i++){
+        printf("The Values: %d\n", i, bun[i]);
+        printf("The address of this elements: %p\n", i, &bun[i]);
+    }
+}
