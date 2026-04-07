70b20b1e0e75f3f4a687188cf2e7c651cec067ce
Kofi Dixon <dicksonsci@gmail.com>
2026-04-07 21:17:16 +0000

    Sorted

--- C++ C files/ray.c
+++ C++ C files/ray.c
@@ -4,7 +4,14 @@ int main(){
     int bun[5] = {5, 5, 6, 6, 6};
 
     for(int i = 0; i < 5; i++){
-        printf("The Values: %d\n", i, bun[i]);
-        printf("The address of this elements: %p\n", i, &bun[i]);
+        // We add a second %d to catch the second variable (bun[i])
+        printf("Index [%d] | Value: %d\n", i, bun[i]);
+        
+        // We add a %d for the index and a %p for the address
+        printf("Index [%d] | Address: %p\n", i, (void*)&bun[i]);
+        
+        printf("--------------------------\n");
     }
+    
+    return 0;
 }
