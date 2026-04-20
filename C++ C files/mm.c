6c8b9d7d6670e2ce86be09f9488226c502a4cc53
Kofi Dixon <dicksonsci@gmail.com>
2026-04-20 20:19:35 +0000

    Matrix multiplication - data entries

---
+++ C++ C files/mm.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(){
+    int a1_1, a1_2, a1_3, a2_1, a2_2, a2_3, a3_1, a3_2, a3_3, b1_1, b1_2, b2_1, b2_2, b3_1, b3_2;
+
+    printf("A matrix solver\n\n");
+    printf("----------------Enter Enteries of A-----------------\n");
+
+    printf("Row 1 column 1:  ");
+    scanf("%d", &a1_1);
+
+    printf("\nRow 1 column 2: ");
+    scanf("%d", &a1_2);
+
+    printf("\nRow 1 column 3: ");
+    scanf("%d", &a1_3);
+
+    printf("\nRow 2 column 1: ");
+    scanf("%d", &a2_1);
+
+    printf("\nRow 2 column 2: ");
+    scanf("%d", &a2_2);
+
+    printf("\nRow 2 column 3: ");
+    scanf("%d", &a2_3);
+
+    printf("\nRow 3 column 1: ");
+    scanf("%d", &a3_1);
+
+    printf("\nRow 3 column 2: ");
+    scanf("%d", &a3_2);
+
+    printf("\nRow 3 column 3: ");
+    scanf("%d", &a3_3);
+
+    printf("----------------Enter entries of B----------------\n\n");
+
+    printf("\nRow 1 column 1: ");
+    scanf("%d", &b1_1);
+    
+    printf("\nRow 1 column 2: ");
+    scanf("%d", &b1_2);
+
+    printf("\nRow 2 column 1: ");
+    scanf("%d", &b2_1);
+
+    printf("\nRow 2 column 2: ");
+    scanf("%d", &b2_2);
+
+    printf("\nRow 3 column 1: ");
+    scanf("%d", &b3_1);
+
+    printf("\nRow 3 column 2: ");
+    scanf("%d", &b3_2);
+
+}
