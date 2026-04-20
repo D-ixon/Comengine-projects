ced532e9ae4922f90a8e359ab922bbb982237d92
Kofi Dixon <dicksonsci@gmail.com>
2026-04-20 20:52:19 +0000

    Matrix Multiplication - Arithmetic of C entries computation

--- C++ C files/mm.c
+++ C++ C files/mm.c
@@ -3,6 +3,8 @@
 int main(){
     int a1_1, a1_2, a1_3, a2_1, a2_2, a2_3, a3_1, a3_2, a3_3, b1_1, b1_2, b2_1, b2_2, b3_1, b3_2;
 
+    int c1_1, c1_2, c2_1, c2_2, c3_1, c3_2;
+
     printf("A matrix solver\n\n");
     printf("----------------Enter Enteries of A-----------------\n");
 
@@ -52,5 +54,22 @@ int main(){
 
     printf("\nRow 3 column 2: ");
     scanf("%d", &b3_2);
+
+    printf("-----------------Witness The Magic------------------\n\n");
+
+    int a = (a1_1 * b1_1) + (a1_2 * b2_1) + (a1_3 * b3_1);
+    int b = (a1_1 * b1_2) + (a1_2 * b2_2) + (a1_3 * b3_2);
+    int c = (a2_1 * b1_1) + (a2_2 * b2_1) + (a2_3 * b3_1);
+    int d = (a2_1 * b1_2) + (a2_2 * b2_2) + (a2_3 * b3_2);
+    int e = (a3_1 * b1_1) + (a3_2 * b2_1) + (a3_3 * b3_1);
+    int f = (a3_1 * b1_2) + (a3_2 * b2_2) + (a2_3 * b3_2);
+
+    printf("C entry row 1 column 1 %d", a);
+    printf("C entry row 1 column 2 %d", b);
+    printf("C entry row 2 column 1 %d", c);
+    printf("C entry row 2 column 2 %d", d);
+    printf("C entry row 3 column 1 %d", e);
+    printf("C entry row 3 column 2 %d", f);
+
 
 }
