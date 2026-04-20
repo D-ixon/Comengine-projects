00bbe4ca91c38ed978a3aa386a874a1fc9091fb8
Kofi Dixon <dicksonsci@gmail.com>
2026-04-20 21:00:31 +0000

    Bug fixed

--- C++ C files/mm.c
+++ C++ C files/mm.c
@@ -62,7 +62,7 @@ int main(){
     int c = (a2_1 * b1_1) + (a2_2 * b2_1) + (a2_3 * b3_1);
     int d = (a2_1 * b1_2) + (a2_2 * b2_2) + (a2_3 * b3_2);
     int e = (a3_1 * b1_1) + (a3_2 * b2_1) + (a3_3 * b3_1);
-    int f = (a3_1 * b1_2) + (a3_2 * b2_2) + (a2_3 * b3_2);
+    int f = (a3_1 * b1_2) + (a3_2 * b2_2) + (a3_3 * b3_2);
 
     printf("C entry row 1 column 1 %d\n", a);
     printf("C entry row 1 column 2 %d\n", b);
